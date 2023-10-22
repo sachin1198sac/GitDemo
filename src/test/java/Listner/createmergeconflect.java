27dda9730375896b3147165f612db49df347a81a
sachin1198sac <sachincsrb010@gmail.com>
2023-10-22 20:15:36 +0530

    now commited to check merge conflect

---
+++ src/test/java/Listner/createmergeconflect.java
@@ -0,0 +1,13 @@
+package Listner;
+
+public class createmergeconflect {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("it's merge conflect");
+	
+
+	}
+
+}
--- src/test/java/copyedfiles/datadriven.java
+++ src/test/java/copyedfiles/datadriven.java
@@ -1,5 +1,7 @@
 package copyedfiles;
 
 public class datadriven {
+	public static void main(String[] args) {
+		System.out.println("dddfgfshddf");
 
-}
+}}
